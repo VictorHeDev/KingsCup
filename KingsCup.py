e63c244e644a22f88bb234bebff47801498a30d3
Victor He <81502599+VictorHeDev@users.noreply.github.com>
2021-06-18 09:46:16 -0400

    Update KingsCup.py
    Added some * and blank line dividers to visually improve the game. Also changed some of the card action descriptions.

--- KingsCup.py
+++ KingsCup.py
@@ -1,17 +1,25 @@
 import random, sys
 
+print('*************************************************************************************')
 print('Welcome to King\'s Cup, also known as the Ring of Fire')
+print('')
+print('Players will take turns picking cards until all 4 Kings are drawn.')
+print('When the last King appears, the person who picked it up must finish the King\'s Cup!')
+print('*************************************************************************************')
+
 numCards = 52
 numKings = 4
-# TODO: 52 cards in a deck and shuffle function
 curDeck = list(range(1, 53))
 random.shuffle(curDeck)
 
-# TODO: While the main (while) loop for the game
 while numCards > 1 and numKings > 1:
+    print('*************************************************************************************')
+    print('')
     print('There are currently: %s Kings and %s Cards left in the deck' % (numKings, numCards))
-    #TODO: Obtain player input
+    print('')
     print('Choose your next action (q to Quit vs. any other key to Continue)')
+    print('')
+    print('*************************************************************************************')
     playerAction = input()
     if playerAction == 'q':
         sys.exit() # Quit the program
@@ -21,6 +29,7 @@ while numCards > 1 and numKings > 1:
     # TODO: What is that random card?
     cardPicked = curDeck[0]
     cardAction = cardPicked % 13
+    print('')
     print('You have drawn: ')
     #TODO: Define card actions and subtract from total number of cards in deck
     if cardAction == 0:
@@ -30,46 +39,49 @@ while numCards > 1 and numKings > 1:
             numCards = numCards -1
             numKings = numKings -1
         else:
-            print('So sorry ... You have drawn the Last King. Finish the King\'s Cup!')
+            print('I\'m so sorry ... You have drawn the Last King. Finish the King\'s Cup (with your mates)!')
             sys.exit()
     elif cardAction == 1:
         print('Ace: Waterfall - Don\'t stop drinking until the person to the left of you finishes!')
+        print('Whoever drew the card will start.')
         curDeck.pop(0)
         numCards = numCards -1
     elif cardAction == 2:
-        print('Two: means you drink!')
+        print('Two: means YOU drink!')
         curDeck.pop(0)
         numCards = numCards -1
     elif cardAction == 3:
-        print('Three: is me!')
+        print('Three: is ME!')
         curDeck.pop(0)
         numCards = numCards -1
     elif cardAction == 4:
-        print('Four: touch the floor! Last one to place both hands on the floor drinks.')
+        print('Four: touch the FLOOR! Last person to place both hands on the floor drinks.')
         curDeck.pop(0)
         numCards = numCards -1
     elif cardAction == 5:
-        print('Five: is guys')
+        print('Five: congratulations GUYS drink.')
         curDeck.pop(0)
         numCards = numCards -1
     elif cardAction == 6:
-        print('Six: is chicks')
+        print('Six: Congratulations CHICKS drink.')
         curDeck.pop(0)
         numCards = numCards -1
     elif cardAction == 7:
-        print('Seven: Heaven! Last one to place both hands in the air drinks.')
+        print('Seven: touch HEAVEN! Last person to place both hands in the air drinks.')
         curDeck.pop(0)
         numCards = numCards -1
     elif cardAction == 8:
-        print('Eight: is Mate. Choose a drinking buddy for the rest of the game.')
+        print('Eight: pick a MATE. Choose a drinking buddy for the rest of the game.')
+        print('The "mate" will also drink whenever the person who picked up the Eight drinks.')
         curDeck.pop(0)
         numCards = numCards -1
     elif cardAction == 9:
-        print('Nine: Rhyme - Choose a word to rhyme with and go left. There\'s a penality if you lose!')
+        print('Nine: RHYME - Choose a word to rhyme with and go left. There\'s a penality if you lose!')
         curDeck.pop(0)
         numCards = numCards -1
     elif cardAction == 10:
-        print('Ten: Categories')
+        print('Ten: Categories - Pick a category and the person who cannot think of something related ')
+        print('to that word must take a penalty.')
         curDeck.pop(0)
         numCards = numCards -1
     elif cardAction == 11:
@@ -81,5 +93,5 @@ while numCards > 1 and numKings > 1:
         curDeck.pop(0)
         numCards = numCards -1
 
-# TODO: Zero cards left
-print('Congratulations, you have finished the game! Everyone take a shot!')
+print('Congratulations, you have finished the game!')
+print('Everyone take a shot!')
